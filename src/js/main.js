08b5dd23898e6890a5d9483ab64e89f15e77afe8
jhoni cruz <jhonidevpro@gmail.com>
2024-09-22 14:29:20 -0300

    :sparkles: feat: add smooth scroll to Section

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import initCalc from './modules/calc.js';
 import initMenu from './modules/menu.js';
 import initImc from './modules/imc.js';
+import initScrollTo from './modules/scrollTo.js';
 
 initCalc();
 initMenu();
 initImc();
+initScrollTo();
